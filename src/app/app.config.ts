1fa65f0b5b96da036c12eaeb699d3f773be486e2
Kemal Toygun Yıldız <toyguny@hotmail.com>
2024-08-15 22:58:41 +0300

    Added backend connection services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,31 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { JwtModule } from '@auth0/angular-jwt';
+import { LocalStorageService } from './services/local-storage.service';
+
+export function tokenGetter() {
+  return inject(LocalStorageService).getItem<string>('token');
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      NgxSpinnerModule,
+      JwtModule.forRoot({
+        config: {
+            tokenGetter:()=> inject(LocalStorageService).getItem("token"),
+            allowedDomains:["localhost:7279"]
+        },
+      })
+    ),
+    provideHttpClient(
+      withInterceptorsFromDi()
+    )]
 };
