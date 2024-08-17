ce33d8c13f3f6dffa3c380c305f602cc79037683
Kemal Toygun Yıldız <toyguny@hotmail.com>
2024-08-17 23:40:53 +0300

    Added login component and related files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
