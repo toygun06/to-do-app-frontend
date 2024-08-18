f11382f37f658e0de516f764e31d3a04d87832e5
Kemal Toygun Yıldız <toyguny@hotmail.com>
2024-08-18 19:10:08 +0300

    Added user layout component and role guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
+import { AuthService } from './services/auth.service';
+import { RoleGuard } from './guards/role.guard';
+import { inject } from '@angular/core';
 
 export const routes: Routes = [
   {
@@ -10,5 +14,17 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
-  }
+  },
+  {
+    path: 'user',
+    component: UserLayoutComponent,
+    canActivate: [()=>inject(AuthService).isAuthenticated(),()=>inject(AuthService),RoleGuard],
+    data: { roles: ['User'] },
+    children: [
+      // {
+      //   path: '',
+      //   component: null,
+      // },
+    ],
+  },
 ];
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,96 @@
+import { Injectable, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpService } from './http.service';
+import { LocalStorageService } from './local-storage.service';
+import { Observable, map, of, take } from 'rxjs';
+import { SwalService } from './swal.service';
+import { IsUserVerifiedModel } from '../models/isUserVerifiedModel';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(
+    private router: Router,
+    private jwtHelper: JwtHelperService,
+    private http: HttpService,
+    private localStorage: LocalStorageService,
+    private swal: SwalService
+  ) {}
+
+  isAuthenticated(): Observable<boolean> {
+    const token: string | null = this.localStorage.getItem('token');
+
+    if (!token) {
+      this.router.navigateByUrl('/login');
+      return new Observable((observer) => observer.next(false));
+    }
+
+    const expired: boolean = this.isTokenExpired(token);
+
+    if (expired) {
+      this.router.navigateByUrl('/login');
+      return new Observable((observer) => observer.next(false));
+    }
+      return new Observable((observer) => observer.next(true));
+  }
+
+  private isTokenExpired(token: string): boolean {
+    return this.jwtHelper.isTokenExpired(token);
+  }
+
+  getUserRole(): string | null {
+    const token:string = this.localStorage.getItem('token');
+    if (token) {
+      const decodedToken: any = this.jwtHelper.decodeToken(token);
+      console.log(decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'])
+      return decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];  // JWT içindeki role claim'i kontrol edin
+
+    }
+    return null;
+  }
+
+  // isAdmin(): boolean {
+  //   return this.getUserRole() === 'Admin';
+  // }
+
+  isUser(): boolean {
+    return this.getUserRole() === 'User';
+  }
+
+  // isDoctor(): boolean {
+  //   return this.getUserRole() === 'Doctor';
+  // }
+
+  // isUserVerified(): Observable<boolean>  {
+  //   return this.http.get<IsUserVerifiedModel>('Auth/IsEmailVerified')
+  //     .pipe(
+  //       map(res => {
+  //         if (res.isEmailVerified) {
+  //           this.localStorage.setItem('userId', res.userId.toString());
+  //           return true;
+  //         } else {
+  //           this.router.navigateByUrl('/verificationcode');
+  //           return false;
+  //         }
+  //       })
+  //     );
+  // }
+
+  logout() {
+    this.swal.callSwal(
+      'Çıkış Yapılacaktır',
+      'Sistemden çıkmak istediğinize eminmisiniz?',
+      () => {
+        this.localStorage.clear();
+        this.router.navigateByUrl('');
+        this.swal.callToast('Çıkış işlemi başarılı.', 'success');
+      },
+      'Evet'
+    );
+  }
+  get isAuthenticatedByUserId(): number {
+    return parseInt(this.localStorage.getItem('userId') || '0');
+  }
+}
